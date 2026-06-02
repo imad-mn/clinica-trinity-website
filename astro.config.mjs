c45b859d9bed965ce8da598a0b975e8798d72592
Imad Marmoud <imadmarmoud@gmail.com>
2026-06-02 12:03:25 -0600

    Remover @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clinicatrinity.com',
@@ -16,5 +14,4 @@ export default defineConfig({
     }
   },
   integrations: [icon(), sitemap()],
-  adapter: cloudflare()
 });
